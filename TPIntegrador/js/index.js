9633bb6197377075e32c2ae9f2247f28ef04c27c
unknown <gloisa@alumno.unlam.edu.ar>
2021-07-01 18:18:59 -0300

    detalle

--- TPIntegrador/js/index.js
+++ TPIntegrador/js/index.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){ 
+
+    if(!localStorage.getItem('carrito')){
+        $("#contador").text('0');
+    }else{
+        $("#contador").text(localStorage.getItem('carrito'));
+    }
+
+
     $("a.desplegable").click(function(){
       if($('.menu').is(":visible")){
           $('.menu').slideUp();
@@ -6,18 +14,53 @@ $(document).ready(function(){
         $('.menu').slideDown();
       }
     });
-$(window).resize(function(){
-    if($(this).width()>768){
-        $(".menu").show();
-    } else{
-        $(".menu").hide();
-    }
-});
+    $(window).resize(function(){
+        if($(this).width()>768){
+            $(".menu").show();
+        } else{
+            $(".menu").hide();
+        }
+    });
 
-var contador=0;
-$(".button").click(function(){
-    contador++;
-    $("#contador").text(contador);
- });
+    var contador=0;
+    $(".button").click(function(){
+        contador++;
+        $("#contador").text(contador);
+     });
+
+
+    $(".form-container .add").click(function(e){
+      var clonar=$(this).parent().clone(true);      
+      $(this).parent().after(clonar); 
+      e.preventDefault(); 
+    });
+    
+   
+    $(".form-container .delete").click(function(e){
+      var cantidad = $(".row-filas").toArray().length;
+      if(cantidad>1){
+        $(this).parent().remove();
+      } 
+       e.preventDefault(); 
+    });
+
+    $(".inscripcion").click(function(){
+        contador++;
+        $("#contador").text(contador);
+        var cantidadCompras = $("#contador").text();
+        localStorage.setItem('carrito',cantidadCompras);
+     });
+
+    $("#titulo-contendido").click(function(){
+        if($("#contendido-oculto").is(':visible')){
+            $("#contendido-oculto").hide();
+        }else{
+            $("#contendido-oculto").show();
+        }
+        
+     });
+
+
+
 
 });
